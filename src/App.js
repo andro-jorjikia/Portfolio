08cee1c0995f20bae15bf70b1a95c7550fb70f7d
andro jorjikia <129059061+andro-jorjikia@users.noreply.github.com>
2025-07-12 08:09:36 +0400

    Starting to build Porfolio language: React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import NavBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <NavBar /> */}
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple React application.</p>
+      {/* Add more components or content here */}
     </div>
   );
 }
